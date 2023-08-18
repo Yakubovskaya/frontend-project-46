1ea04821febb517533f3df54bdbad9a530150cdf
Marina Yakubovskaya <maribars93@yandex.ru>
2023-08-18 14:41:24 +0300

    Add description, version & helpOption to program

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
-const program = new Command();
+import { program } from 'commander';
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('output the version number')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
 
 program.parse();
 
