7d0d2a723c809fa748fea936afd270811c60f684
Marina Yakubovskaya <maribars93@yandex.ru>
2023-08-18 14:42:06 +0300

    Add arguments and format option to program

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,9 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('output the version number')
   .helpOption('-h, --help', 'output usage information')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format')
 
 program.parse();
 
